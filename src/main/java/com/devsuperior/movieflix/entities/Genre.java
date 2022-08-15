9bcaeb48a0980c6d32a1fe06c07f82ee8ead21f3
melck <melck_junior@hotmail.com>
2022-08-15 13:33:42 +0100

    Validation and Security

--- src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_name")
+@Table(name = "tb_genre")
 public class Genre implements Serializable {
 
     @Id
--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_g")
+@Table(name = "tb_movie")
 public class Movie implements Serializable {
 
     @Id
